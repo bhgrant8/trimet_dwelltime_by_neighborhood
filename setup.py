af77d3d782a9019e5bd77ffb7fed015a9e20f81d
Brian Grant <brian.grant@urbanairship.com>
2018-11-07 21:07:28 -0800

    initial cookiecutter project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='data science time series analysis looking at bus dwell time by neighborhoods',
+    author='brian grant',
+    license='MIT',
+)
